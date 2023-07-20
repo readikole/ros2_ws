dcb36f22213914b4fdc208fbc1170537cafc6f5c
Realeboga Dikole <rgdikole@gmail.com>
2023-07-20 15:05:19 +0200

    multiple nodes created

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -2,20 +2,36 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
      
      
 class NumberCounterNode(Node): # MODIFY NAME
     def __init__(self):
         super().__init__("number_counter") # MODIFY NAME
         self.counter_=0
+        self.number_count_publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.number_subscriber_ = self.create_subscription(
             Int64, "number", self.callback_number, 10)
+        self.reset_counter_service_ = self.create_service(
+                SetBool, "reset_counter",self.callback_reset_counter )
         self.get_logger().info("Number Counter has been started.")
     
     def callback_number(self, msg):
         self.counter_+=msg.data
-        self.get_logger().info(str(self.counter_))
-     
+        new_msg = Int64()
+        new_msg.data = self.counter_
+        self.number_count_publisher_.publish(new_msg)
+    
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.counter_ =  0
+            response.success=True
+            response.message="Counter has been reset"
+        else:
+            response.success=False
+            response.message= "Counter has not beenn reset"
+        return response
+
 def main(args=None):
     rclpy.init(args=args)
     node = NumberCounterNode() # MODIFY NAME
