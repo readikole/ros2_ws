46e24b59f1661eccfd332ebf00c722862f5b0c91
Realeboga Dikole <rgdikole@gmail.com>
2023-07-24 10:01:47 +0200

    major changes to both python and cpp packages

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -6,26 +6,26 @@ public:
     MyNode() : Node("cpp_test"), counter_(0)
     {
         RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
+
         timer_ = this->create_wall_timer(std::chrono::seconds(1),
-                                         std::bind(&MyNode::timeCallback, this));
+                                         std::bind(&MyNode::timerCallback, this));
     }
 
 private:
-    void timeCallback()
+    void timerCallback()
     {
         counter_++;
         RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
     }
+
     rclcpp::TimerBase::SharedPtr timer_;
     int counter_;
 };
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello Cpp Node");
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
--- src/my_cpp_pkg/src/smartphone.cpp
+++ src/my_cpp_pkg/src/smartphone.cpp
@@ -1,14 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/string.hpp"
-class SmartphoneNode : public rclcpp::Node // MODIFY NAME
+
+class SmartphoneNode : public rclcpp::Node
 {
 public:
-    SmartphoneNode() : Node("smartphone") // MODIFY NAME
+    SmartphoneNode() : Node("smartphone")
     {
         subscriber_ = this->create_subscription<example_interfaces::msg::String>(
             "robot_news", 10,
             std::bind(&SmartphoneNode::callbackRobotNews, this, std::placeholders::_1));
-            RCLCPP_INFO(this->get_logger(), "Smartphone has been started");
+        RCLCPP_INFO(this->get_logger(), "Smartphone has been started.");
     }
 
 private:
@@ -16,13 +17,14 @@ private:
     {
         RCLCPP_INFO(this->get_logger(), "%s", msg->data.c_str());
     }
+
     rclcpp::Subscription<example_interfaces::msg::String>::SharedPtr subscriber_;
 };
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<SmartphoneNode>(); // MODIFY NAME
+    auto node = std::make_shared<SmartphoneNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
